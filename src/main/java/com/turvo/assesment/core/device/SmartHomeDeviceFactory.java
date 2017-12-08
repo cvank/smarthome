e036b92b2230fed1fd406c4cdf960308f95d2862
Chandrashekar V <Chandra.vankayalapati@imaginea.com>
2017-12-08 13:29:26 +0530

    device factory added

--- src/main/java/com/turvo/assesment/core/device/SmartHomeDeviceFactory.java
+++ src/main/java/com/turvo/assesment/core/device/SmartHomeDeviceFactory.java
@@ -5,6 +5,7 @@ package com.turvo.assesment.core.device;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 import com.turvo.assesment.core.EnergySourceType;
 import com.turvo.assesment.core.EnergyType;
@@ -23,10 +24,16 @@ public class SmartHomeDeviceFactory {
 
 	public SmartHomeDevice create(final String deviceName, final Map<EnergyType, List<EnergySourceType>> deviceData) {
 
-		int noOfEnergyTypes = deviceData.size();
+		int noOfEnergyTypes = Objects.isNull(deviceData) ? 0 : deviceData.size();
 		SmartHomeDeviceBuilder builder = null;
+		
+		/**
+		 * If only one energy type is being used by a device then it is considered as
+		 * single consumption device.
+		 */
 		switch (noOfEnergyTypes) {
-		case 1 | 0:
+		case 1:
+		case 0:
 
 			builder = new SingleConsumptionDeviceBuilder();
 
